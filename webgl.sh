96dad14815204598c1c4aa480881ecda9fdfb05d
JaeHoon Lee <jaehoonlee88@gmail.com>
2015-03-01 00:29:14 +0900

    webgl.sh added

---
+++ webgl.sh
@@ -0,0 +1,15 @@
+case "$1" in 
+    start)
+	echo "=========================================Starting WebGL Server============================================"
+	nohup python manage.py runserver 0.0.0.0:8000 &
+	LASTPID=$!
+	echo $LASTPID > webGL.pid
+    ;;
+    stop)
+	echo "=========================================Stoping WebGL Server============================================"
+	CAMEROPID=$(cat /root/camero/webGL.pid)
+	echo $CAMEROPID
+	pkill -9 -P $CAMEROPID
+    ;;
+esac
+exit 0
